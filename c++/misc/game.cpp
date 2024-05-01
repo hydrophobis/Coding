5c80f344e3dfb65c4771fe827612d49de2bd56e6
CamoGekko <142858559+CamoGekko@users.noreply.github.com>
2024-05-01 11:00:12 -0500

    s

---
+++ c++/misc/game.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    
+    struct Entity {
+        string name;
+        int health;
+        int damage;
+    }
+
+    struct Player : public Entity {
+        vector<>
+    }
+}
