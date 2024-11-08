4f1b65846d31ba5c0390e4f524bc531336b126e1
hydrophobis <142858559+hydrophobis@users.noreply.github.com>
2024-11-08 09:49:34 -0600

    Update cplus.h

--- c/cplus.h
+++ c/cplus.h
@@ -1,51 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-#define stracc(s) s.internal
+#define boolacc(b) b.field
+#define boolcons(val) ((bool){val})
+struct bool {
+    unsigned int field : 1;
+};
+typedef struct bool bool;
+void bprint(bool in) {
+    if(boolacc(in) == 1) {
+        printf("true");
+    } else if(boolacc(in) == 0) {
+        printf("false");
+    } else {
+        printf("invalid boolean value");
+    }
+}
+
+#define stracc(s) s.field
+#define strcons(s) {s}
 struct string {
-    char *internal;
-
-    int contains(char ch) {
-        for (int i = 0; i < strlen(internal); i++) {
-            if (internal[i] == ch) {
-                return 1;
-            }
-        }
-        return 0;
-    }
-
-    int contains(char *ch) {
-        for (int i = 0; i < strlen(internal); i++) {
-            for (int j = 0; j < strlen(ch); j++) {
-                if (internal[i] == ch[j]) {
-                    return 1;
-                }
-            }
-        }
-        return 0;
-    }
+    char *field;
 };
 typedef struct string string;
-string string_init(char* str) {
-    string out;
-    out.internal = (char*)malloc(strlen(str) * sizeof(char));
-    out.internal = str;
-    return out;
-}
-bool findstr(char* str, char* search){
-    int counter = 0;
-    for(int i = 0; i < str.length(); i++;){
-        if(str[i] == search[counter]){
-            counter += 1;
-        } else {
-            counter = 0;
-        }
-        
-        if(i == str.length() - 1){
-            return false;
+bool contains_char(const struct string *s, const char ch) {
+    bool out = boolcons(1);
+    int len = strlen(s->field);  // Store length once
+    for (int i = 0; i < len; i++) {
+        if (s->field[i] == ch) {
+            return out;
         }
     }
-    return true;
+    out = boolcons(0);
+    return out;
 }
-    
+bool contains_any(const struct string *s, const char *chars) {
+    bool out = boolcons(1);
+    for (int i = 0; i < strlen(s->field); i++) {
+        for (int j = 0; j < strlen(chars); j++) {
+            if (s->field[i] == chars[j]) {
+                return out;
+            }
+        }
+    }
+    out = boolcons(0);
+    return out;
+}
